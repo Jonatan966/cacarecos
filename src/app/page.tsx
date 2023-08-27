ea4d8032e596fb3ec65b0b549f1d5138c1910e4a
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-08-27 13:38:02 -0300

    feat: feed content carousel on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,31 @@
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
 import { ContentCarousel } from "@/components/molecules/content-carousel";
 import { CategoriesSection } from "@/components/organisms/categories-section";
 import { ProductsSection } from "@/components/organisms/products-section";
 import { hygraphService } from "@/services/hygraph";
+import { generateCarouselActionLink } from "@/utils/generate-carousel-action-link";
 
 export default async function Home() {
-  const { categories, products } = await hygraphService.getHomeResume();
+  const { categories, products, carousels } =
+    await hygraphService.getHomeResume();
 
   return (
     <>
       <ContentCarousel
-        items={[
-          {
-            imageUrl: "/vercel.svg",
-            description: "Descrição interessante",
-            action: <Button>Ação</Button>,
-          },
-          {
-            imageUrl: "/vercel.svg",
-          },
-          {
-            imageUrl: "/vercel.svg",
-          },
-        ]}
+        items={carousels.map((carousel) => ({
+          imageUrl:
+            carousel?.image?.url ||
+            carousel.refereal_product?.images[0].url ||
+            "/vercel.svg",
+          description: carousel.title,
+          action: (
+            <Link href={generateCarouselActionLink(carousel)}>
+              <Button className="w-full block">{carousel.cta}</Button>
+            </Link>
+          ),
+        }))}
         isPlaying
         className="h-96"
       />
