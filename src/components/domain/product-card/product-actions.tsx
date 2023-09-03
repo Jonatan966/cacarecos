700ede430706a45c718d91d0f83cadd4f01b4d32
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-09-03 18:39:05 -0300

    feat: set able to go to checkout

--- src/components/domain/product-card/product-actions.tsx
+++ src/components/domain/product-card/product-actions.tsx
@@ -3,13 +3,17 @@
 import { useCart } from "@/components/providers/cart-provider";
 import { Button } from "@/components/ui/button";
 import { Product } from "@/entities/product";
-import { Heart, ShoppingBag } from "lucide-react";
+import { metaService } from "@/services/meta";
+import { stripeJsService } from "@/services/stripe-js";
+import { Heart, Loader, ShoppingBag } from "lucide-react";
+import { useState } from "react";
 
 interface ProductActionsProps {
   product: Product;
 }
 
 export function ProductActions({ product }: ProductActionsProps) {
+  const [isRedirecting, setIsRedirecting] = useState(false);
   const { toggleProductFromCart, cart } = useCart();
 
   function handleCartAction(event: React.MouseEvent<HTMLButtonElement>) {
@@ -17,10 +21,37 @@ export function ProductActions({ product }: ProductActionsProps) {
     toggleProductFromCart(product);
   }
 
+  async function handleCheckout(event: React.MouseEvent) {
+    event.preventDefault();
+
+    setIsRedirecting(true);
+
+    try {
+      const checkoutSessionId = await metaService.checkout({
+        products: [
+          {
+            priceId: product.priceId,
+            quantity: 1,
+          },
+        ],
+      });
+
+      await stripeJsService.goToCheckout(checkoutSessionId);
+    } catch (e) {
+      console.log(e);
+    }
+
+    setIsRedirecting(false);
+  }
+
   return (
     <div className="flex space-x-1">
-      <Button className="flex-1" onClick={(e) => e.preventDefault()}>
-        Comprar
+      <Button
+        className="flex-1"
+        onClick={handleCheckout}
+        disabled={isRedirecting}
+      >
+        {isRedirecting ? <Loader className="animate-spin" /> : "Comprar"}
       </Button>
       <Button
         size="icon"
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,9 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/products(.*)", "/cart"],
+  ignoredRoutes: ["/api/(.*)"],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
