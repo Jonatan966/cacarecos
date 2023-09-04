de8b04ec8dda5d440fb3e7c68cbeea88e7ee04ea
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-09-03 21:42:03 -0300

    fix: ignore localstorage on server

--- src/services/local-storage/retrieve-item.ts
+++ src/services/local-storage/retrieve-item.ts
@@ -1,6 +1,10 @@
 import { StorageKey, StorageValues } from "./types";
 
 export function retrieveItem<K extends StorageKey>(key: K) {
+  if (typeof window === "undefined") {
+    return;
+  }
+
   const item = localStorage.getItem(key);
 
   if (!item) {
