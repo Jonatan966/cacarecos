f9f491445c66d61accfcbfb3a6bd4693b09696fe
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-09-30 14:54:48 -0300

    feat: put products ids into stripe session

--- src/services/stripe/create-checkout-session.ts
+++ src/services/stripe/create-checkout-session.ts
@@ -1,9 +1,11 @@
 import { stripeApi } from "./api";
+import Stripe from "stripe";
 
 interface CreateCheckoutSessionProps {
   products: {
     quantity: number;
     priceId: string;
+    id: string;
   }[];
   customerId: string;
 }
@@ -12,6 +14,18 @@ export async function createCheckoutSession({
   products,
   customerId,
 }: CreateCheckoutSessionProps) {
+  const lineItems: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+  const productsIDs: string[] = [];
+
+  for (const product of products) {
+    lineItems.push({
+      price: product.priceId,
+      quantity: product.quantity,
+    });
+
+    productsIDs.push(product.id);
+  }
+
   const session = await stripeApi.checkout.sessions.create({
     line_items: products.map((product) => ({
       price: product.priceId,
@@ -24,6 +38,9 @@ export async function createCheckoutSession({
     payment_method_types: ["card"],
     shipping_address_collection: {
       allowed_countries: ["BR"],
+    },
+    metadata: {
+      productsIDs: JSON.stringify(productsIDs),
     },
   });
 
