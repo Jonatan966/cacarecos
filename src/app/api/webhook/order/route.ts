e99d6cc28aad76398c820fb552022512bef5c62f
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-10-29 20:47:50 -0300

    feat: add stripe order creation webhook

---
+++ src/app/api/webhook/order/route.ts
@@ -0,0 +1,46 @@
+import { OrderProduct } from "@/entities/order";
+import { hygraphService } from "@/services/hygraph";
+import { stripeApi } from "@/services/stripe/api";
+import { NextRequest, NextResponse } from "next/server";
+import { Stripe } from "stripe";
+
+export async function POST(req: NextRequest) {
+  const signature = req.headers.get("stripe-signature") || "";
+
+  const payload = await req.text();
+
+  const event = stripeApi.webhooks.constructEvent(
+    payload,
+    signature,
+    process.env.STRIPE_SESSION_COMPLETED_WEBHOOK_SIGN!
+  );
+
+  if (event.type !== "checkout.session.completed") {
+    return new NextResponse(undefined, {
+      status: 400,
+    });
+  }
+
+  const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+  const orderProducts = JSON.parse(
+    checkoutSession.metadata?.orderProducts || "[]"
+  ) as OrderProduct[];
+
+  if (!checkoutSession.customer) {
+    return new NextResponse(undefined, {
+      status: 400,
+    });
+  }
+
+  await hygraphService.addOrder({
+    checkoutId: checkoutSession.id,
+    customerId: String(checkoutSession.customer),
+    totalAmount: checkoutSession.amount_total!,
+    products: orderProducts,
+  });
+
+  return new NextResponse(undefined, {
+    status: 204,
+  });
+}
