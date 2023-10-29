5b84e624c03f1eda968c22a4aa168bc255feceee
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-10-28 21:39:57 -0300

    feat: improve orders table

--- src/components/domain/orders-table/index.tsx
+++ src/components/domain/orders-table/index.tsx
@@ -1,18 +1,7 @@
 "use client";
 
-import * as React from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { DotsHorizontalIcon } from "@radix-ui/react-icons";
-import {
-  ColumnDef,
-  ColumnFiltersState,
-  PaginationState,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -27,233 +16,159 @@ import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
+  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
 import { Badge } from "@/components/ui/badge";
+import Image from "next/image";
+import { metaService } from "@/services/meta";
+import { Order } from "@/entities/order";
+import { formatCurrency } from "@/utils/format-currency";
+import { Loader } from "lucide-react";
+import { formatDate } from "@/utils/format-date";
+import { Pagination } from "@/entities/pagination";
 
-const data: Order[] = [
-  {
-    id: "m5gr84i9",
-    amount: 316,
-    status: "success",
-    products: "Produto tal",
-    createdAt: "30/09/2023",
-  },
-  {
-    id: "3u1reuv4",
-    amount: 242,
-    status: "success",
-    products: "Produto tal",
-    createdAt: "30/09/2023",
-  },
-  {
-    id: "derv1ws0",
-    amount: 837,
-    status: "processing",
-    products: "Produto tal",
-    createdAt: "30/09/2023",
-  },
-  {
-    id: "5kma53ae",
-    amount: 874,
-    status: "success",
-    products: "Produto tal",
-    createdAt: "30/09/2023",
-  },
-  {
-    id: "bhqecj4p",
-    amount: 721,
-    status: "failed",
-    products: "Produto tal",
-    createdAt: "30/09/2023",
-  },
-];
-
-export type Order = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  products: string;
-  createdAt: string;
-};
-
-export const columns: ColumnDef<Order>[] = [
-  {
-    accessorKey: "createdAt",
-    header: "Criado Em",
-  },
-  {
-    accessorKey: "products",
-    header: "Produtos",
-    cell: ({ row }) => (
-      <div className="lowercase">{row.getValue("products")}</div>
-    ),
-  },
-  {
-    accessorKey: "status",
-    header: "Status",
-    cell: ({ row }) => <Badge>{row.getValue("status")}</Badge>,
-  },
-  {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Valor Total</div>,
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"));
-
-      // Format the amount as a dollar amount
-      const formatted = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL",
-      }).format(amount);
-
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
-  },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const payment = row.original;
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <DotsHorizontalIcon className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
-];
+interface Filters {
+  page: number;
+  search: string;
+}
 
 export function OrdersTable() {
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-    []
-  );
-  const [pagination, setPagination] = React.useState<PaginationState>({
-    pageIndex: 0,
-    pageSize: 10,
+  const [isLoadingOrders, setIsLoadingOrders] = useState(true);
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [pagination, setPagination] = useState<Pagination>();
+  const [filters, setFilters] = useState<Filters>({
+    page: 0,
+    search: "",
   });
 
-  const table = useReactTable({
-    data,
-    columns,
-    onColumnFiltersChange: setColumnFilters,
-    onPaginationChange: setPagination,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    state: {
-      columnFilters,
-      pagination,
-    },
-  });
+  const searchDelayIdRef = useRef(-1);
+
+  function onGoToPreviousPage() {
+    setFilters((oldFilters) => ({ ...oldFilters, page: oldFilters.page - 1 }));
+  }
+
+  function onGoToNextPage() {
+    setFilters((oldFilters) => ({ ...oldFilters, page: oldFilters.page + 1 }));
+  }
+
+  function handleSearch(evt: ChangeEvent<HTMLInputElement>) {
+    clearTimeout(searchDelayIdRef.current);
+
+    searchDelayIdRef.current = Number(
+      setTimeout(() => setFilters({ page: 0, search: evt.target.value }), 250)
+    );
+  }
+
+  useEffect(() => {
+    setIsLoadingOrders(true);
+
+    metaService
+      .getOrders(filters)
+      .then((ordersResponse) => {
+        setOrders(ordersResponse.orders);
+        setPagination(ordersResponse.pageInfo);
+      })
+      .finally(() => setIsLoadingOrders(false));
+  }, [filters]);
 
   return (
     <div className="w-full">
       <div className="flex items-center py-4">
         <Input
           placeholder="Filtrar pedidos..."
-          value={
-            (table.getColumn("products")?.getFilterValue() as string) ?? ""
-          }
-          onChange={(event) =>
-            table.getColumn("products")?.setFilterValue(event.target.value)
-          }
           className="flex-1"
+          onChange={handleSearch}
         />
       </div>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  Nenhum resultado encontrado.
-                </TableCell>
-              </TableRow>
+
+      {isLoadingOrders ? (
+        <Loader className="animate-spin mx-auto" size={32} />
+      ) : (
+        <>
+          <Table>
+            {!isLoadingOrders && !orders.length && (
+              <TableCaption>
+                Não há pedidos que correspondem sua busca
+              </TableCaption>
             )}
-          </TableBody>
-        </Table>
-      </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
-          Página {pagination.pageIndex + 1} de {table.getPageCount()}
-        </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Próximo
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Anterior
-          </Button>
-        </div>
-      </div>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Criado Em</TableHead>
+                <TableHead>Produtos</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Valor Total</TableHead>
+                <TableHead></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {orders.map((order) => (
+                <TableRow key={order.id}>
+                  <TableCell>{formatDate(new Date(order.createdAt))}</TableCell>
+                  <TableCell>
+                    <div className="flex">
+                      {order.products.map((product) => (
+                        <Image
+                          key={product.id}
+                          src={product.images[0].url}
+                          width={64}
+                          height={64}
+                          alt={product.slug}
+                          className="h-[32px] object-contain"
+                        />
+                      ))}
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <Badge>{order.orderStatus}</Badge>
+                  </TableCell>
+                  <TableCell>{formatCurrency(order.totalAmount)}</TableCell>
+                  <TableCell width={16}>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                          <span className="sr-only">Open menu</span>
+                          <DotsHorizontalIcon className="h-4 w-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem>Copy payment ID</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View customer</DropdownMenuItem>
+                        <DropdownMenuItem>
+                          View payment details
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <div className="flex justify-end gap-1 mt-1">
+            <Button
+              size="sm"
+              variant="outline"
+              disabled={!pagination?.hasPreviousPage}
+              onClick={onGoToPreviousPage}
+            >
+              Anterior
+            </Button>
+            <Button
+              size="sm"
+              variant="outline"
+              disabled={!pagination?.hasNextPage}
+              onClick={onGoToNextPage}
+            >
+              Próximo
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/entities/pagination.ts
@@ -0,0 +1,5 @@
+export interface Pagination {
+  hasNextPage: boolean;
+  pageSize: number;
+  hasPreviousPage: boolean;
+}
