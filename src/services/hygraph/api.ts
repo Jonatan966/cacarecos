c67ba1df705dd74603358a73ce0a553fb864d9bd
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-10-29 19:53:03 -0300

    feat: use api token in hygraph api

--- src/services/hygraph/api.ts
+++ src/services/hygraph/api.ts
@@ -1,5 +1,10 @@
 import { GraphQLClient } from "graphql-request";
 
 export const hygraphApi = new GraphQLClient(
-  "https://api-sa-east-1.hygraph.com/v2/cllrhrldb2l6o01uecgswb2fn/master"
+  "https://api-sa-east-1.hygraph.com/v2/cllrhrldb2l6o01uecgswb2fn/master",
+  {
+    headers: {
+      authorization: process.env.HYGRAPH_API_TOKEN!,
+    },
+  }
 );
