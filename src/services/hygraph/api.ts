0bdf69ee2056b169f312809be6ad2032c1d3cf6c
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-10-29 22:21:26 -0300

    refact: add hygraph api id into env

--- src/services/hygraph/api.ts
+++ src/services/hygraph/api.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from "graphql-request";
 
 export const hygraphApi = new GraphQLClient(
-  "https://api-sa-east-1.hygraph.com/v2/cllrhrldb2l6o01uecgswb2fn/master",
+  `https://api-sa-east-1.hygraph.com/v2/${process.env.HYGRAPH_API_ID}/master`,
   {
     headers: {
       authorization: process.env.HYGRAPH_API_TOKEN!,
